e95e6ea04d0f9278dcec66e93866b05ae7112456
Tony Arkles <tony@persea.ca>
2021-02-03 07:40:07 -0600

    Added example passing parameters to constructor

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "activeobject.h"
 #include <unistd.h>
 
@@ -12,6 +13,13 @@ public:
   void run(bool* keep_running);
 };
 
+class AO3 {
+public:
+  AO3(std::string name);
+  void run(bool* keep_running);
+private:
+  std::string name;
+};
 
 void AO1::run(bool* keep_running) {
   while(*keep_running) {
@@ -29,13 +37,25 @@ void AO2::run(bool* keep_running) {
   std::cout << "AO2 done" << std::endl;
 }
 
+AO3::AO3(std::string name) : name(name) {
+
+}
+
+void AO3::run(bool* keep_running) {
+  while(*keep_running) {
+    sleep(1);
+    std::cout << "AO3 named: " << name << std::endl;
+  }
+  std::cout << "AO3 done" << std::endl;
+}
+
 int main(int argc, char* argv[]) {
   (void)argc;
   (void)argv;
 
   auto ao1 = make_ao<AO1>();
   auto ao2 = make_ao<AO2>();
-
+  auto ao3 = make_ao<AO3>("Charlie");
   sleep(5);
   std::cout << "main done" << std::endl;
 }
